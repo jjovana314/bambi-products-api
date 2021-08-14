9841841bf43fb5c07e66b6249098a10911cde3e1
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-14 11:56:18 +0200

    typos in imports fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginModule } from './login/login.module';
 import config from './config/address';
 import { AdminModule } from './admin/admin.module';
-import { ProductModule } from './product/product.module';
 import { ProductsModule } from './products/products.module';
 import { RegisterModule } from './register/register.module';
+import { ProductClassModule } from './productClass/productClass.module';
 
 @Module({
     imports: [
       LoginModule,
       AdminModule,
-      ProductModule,
       ProductsModule,
+      ProductClassModule,
       RegisterModule,
       MongooseModule.forRoot(config.mongodbURI,     // note: using docker ip address: 172.17.0.1
       { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }),
