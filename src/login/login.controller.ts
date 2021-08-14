31888eab8e0753bb258342477feb2d9a1c3bd3ad
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-14 12:16:51 +0200

    code refactored

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -6,44 +6,44 @@ import { RegisterService } from 'src/register/register.service';
 
 @Controller('login')
 export class LoginController {
-	constructor(private readonly loginService: LoginService,
-    private readonly registerService: RegisterService) {}
+  constructor(private readonly loginService: LoginService,
+    private readonly registerService: RegisterService) { }
 
-	@Post()
-	async login(@Body() loginInfo: LoginDto): Promise<Login> {
-		await this.registerService.validateLogin(
-			loginInfo.username, loginInfo.password
-		)
-		const result = this.loginService.login(loginInfo);
-		var date = new Date().toLocaleString('hu-HU', {timeZone: 'CET'});
-		console.log(
-			`User: ${loginInfo.username} logged in at: ${date}`
-		)
-		return await result;
-	}
+  @Post()
+  async login(@Body() loginInfo: LoginDto): Promise<Login> {
+    await this.registerService.validateLogin(
+      loginInfo.username, loginInfo.password
+    )
+    const result = this.loginService.login(loginInfo);
+    var date = new Date().toLocaleString('hu-HU', { timeZone: 'CET' });
+    console.log(
+      `User: ${loginInfo.username} logged in at: ${date}`
+    )
+    return await result;
+  }
 
-	@Get(':id')
-	findUser(@Param('id') id): Promise<Login> {
+  @Get(':id')
+  findUser(@Param('id') id): Promise<Login> {
     return this.loginService.findUser(id);
-	}
+  }
 
-	@Get('findByUsername/:username')
-	async findUserByUsername(@Param('username') username): Promise<Login> {
+  @Get('findByUsername/:username')
+  async findUserByUsername(@Param('username') username): Promise<Login> {
     await this.loginService.userExist(username);
     return await this.loginService.findByUsername(username);
-	}
+  }
 
-	@Get()
-	async listAllUsers() {
+  @Get()
+  async listAllUsers() {
     return await this.loginService.listAllUsers();
-	}
+  }
 
   @Get('logout/:_id')
   async logout(@Param('_id') _id: string) {
     await this.loginService.logout(_id);
-    var date = new Date().toLocaleString('hu-HU', {timeZone: 'CET'});
+    var date = new Date().toLocaleString('hu-HU', { timeZone: 'CET' });
     console.log(
-			`User with id: ${_id} logged out at: ${date}`
-		);
+      `User with id: ${_id} logged out at: ${date}`
+    );
   }
 }
