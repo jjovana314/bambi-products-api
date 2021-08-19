2d132372e3adcdd65b1df5654e955a97a93d4a74
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-19 15:09:57 +0200

    registerModel get method added, registerModel field renamed into _registerModel

--- src/register/register.service.ts
+++ src/register/register.service.ts
@@ -1,10 +1,8 @@
 import { HttpCode, HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, ObjectId } from 'mongoose';
+import { Model } from 'mongoose';
 import { Register } from './interfaces/register.interface';
 import { RegisterDto } from './dto/register.interface.dto';
-import { Observable, from, of, switchMap, lastValueFrom } from 'rxjs';
-import { boolean } from 'yargs';
 import { UpdatePasswordDto } from './dto/update.password.interface.dto';
 import { UpdateUsernameDto } from './dto/update.username.interface.dto';
 
@@ -14,13 +12,17 @@ const saltRounds = 12;
 @Injectable()
 export class RegisterService {
   constructor(
-    @InjectModel('Register') private readonly registerModel: Model<Register>
+    @InjectModel('Register') private readonly _registerModel: Model<Register>
   ) { }
+
+  get registerModel() {
+    return this._registerModel;
+  }
 
   async registerUser(userData: RegisterDto): Promise<Register> {
     const passHashed = await bcrypt.hash(userData.password, saltRounds);
     // put hashed password in userData
-    const user = await this.registerModel.findOne({ username: userData.username });
+    const user = await this._registerModel.findOne({ username: userData.username });
     if (user) {
       throw new HttpException(
         `User with username: ${userData.username} already exists.`,
@@ -31,7 +33,7 @@ export class RegisterService {
       ...userData,
       password: passHashed
     };
-    const newUser = new this.registerModel(userData);
+    const newUser = new this._registerModel(userData);
     return await newUser.save();
   }
 
@@ -43,7 +45,7 @@ export class RegisterService {
   }
 
   async isRegistered(username: string) {
-    var user = await this.registerModel.findOne({
+    var user = await this._registerModel.findOne({
       username: username
     });
     if (!user) {
@@ -55,23 +57,12 @@ export class RegisterService {
     return await user;
   }
 
-  async validateLogin(username: string, password: string) {
-    var user = await this.isRegistered(username);
-    var passwordsEqual = await bcrypt.compare(password, user.password);
-    if (!passwordsEqual) {
-      throw new HttpException(
-        `Password for ${(await user).username} is not valid.`,
-        HttpStatus.BAD_REQUEST
-      );
-    }
-  }
-
   async deleteUser(id: string) {
-    await this.registerModel.findOneAndRemove({ _id: id });
+    await this._registerModel.findOneAndRemove({ _id: id });
   }
 
   async updatePassword(id: string, updateData: UpdatePasswordDto): Promise<Register> {
-    const userData = (await this.registerModel.findById(id)).toObject();
+    const userData = (await this._registerModel.findById(id)).toObject();
     const passwordsEqual = await bcrypt.compare(
       updateData.newPassword, userData.password
     );
@@ -82,7 +73,7 @@ export class RegisterService {
       const newPassHashed = await bcrypt.hash(
         updateData.newPassword, saltRounds
       );
-      return await this.registerModel.findOneAndUpdate(
+      return await this._registerModel.findOneAndUpdate(
         { username: userData.username },
         { $set: { password: newPassHashed } }
       );
@@ -90,7 +81,7 @@ export class RegisterService {
   }
 
   async updateUsername(id: string, updateData: UpdateUsernameDto): Promise<Register> {
-    const userData = (await this.registerModel.findById(id)).toObject();
+    const userData = (await this._registerModel.findById(id)).toObject();
     const passwordsEqual = bcrypt.compare(updateData.password, userData.password)
     if (!passwordsEqual) {
       throw new HttpException(
@@ -101,7 +92,7 @@ export class RegisterService {
     if (userData.username == updateData.newUsername) {
       throw new HttpException('Please enter new username', HttpStatus.BAD_REQUEST);
     }
-    return await this.registerModel.findOneAndUpdate(
+    return await this._registerModel.findOneAndUpdate(
       { username: userData.username },
       { $set: { username: updateData.newUsername } }
     );
