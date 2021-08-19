dcde11c59cdd2aae59535af55c32744b286b4f56
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-19 15:08:58 +0200

    module updated, service exported

--- src/register/register.module.ts
+++ src/register/register.module.ts
@@ -4,11 +4,14 @@ import { RegisterController } from "./register.controller";
 import { RegisterService } from "./register.service";
 import { RegisterSchema } from "./schemas/register.schema";
 
-
 @Module({
-    imports: [MongooseModule.forFeature([{name: 'Register', schema: RegisterSchema}])],
-    controllers: [RegisterController],
-    providers: [RegisterService],
-    exports: [RegisterService]
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Register', schema: RegisterSchema }
+    ])
+  ],
+  controllers: [RegisterController],
+  providers: [RegisterService],
+  exports: [RegisterService]
 })
-export class RegisterModule {} 
+export class RegisterModule { }
