aa3110111f5eb3b276e483589c2a527707603517
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-19 15:07:51 +0200

    methods updated, LocalStrategy imported

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -2,20 +2,24 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { LoginDto } from './dto/login.interface.dto';
 import { LoginService } from './login.service';
 import { Login } from './interfaces/login.interface';
-import { RegisterService } from 'src/register/register.service';
+import { LocalStrategy } from 'src/auth/auth.local.strategy';
 
 @Controller('login')
 export class LoginController {
-  constructor(private readonly loginService: LoginService,
-    private readonly registerService: RegisterService) { }
+  constructor(
+    private readonly loginService: LoginService,
+    private readonly localStrategy: LocalStrategy
+  ) { }
 
   @Post()
   async login(@Body() loginInfo: LoginDto): Promise<Login> {
-    await this.registerService.validateLogin(
+    await this.localStrategy.validate(
       loginInfo.username, loginInfo.password
     )
     const result = this.loginService.login(loginInfo);
-    var date = new Date().toLocaleString('hu-HU', { timeZone: 'CET' });
+    var date = new Date().toLocaleString(
+      'hu-HU', { timeZone: 'CET' }
+    );
     console.log(
       `User: ${loginInfo.username} logged in at: ${date}`
     )
@@ -28,7 +32,9 @@ export class LoginController {
   }
 
   @Get('findByUsername/:username')
-  async findUserByUsername(@Param('username') username): Promise<Login> {
+  async findUserByUsername(
+    @Param('username') username
+  ): Promise<Login> {
     await this.loginService.userExist(username);
     return await this.loginService.findByUsername(username);
   }
@@ -41,7 +47,9 @@ export class LoginController {
   @Get('logout/:_id')
   async logout(@Param('_id') _id: string) {
     await this.loginService.logout(_id);
-    var date = new Date().toLocaleString('hu-HU', { timeZone: 'CET' });
+    var date = new Date().toLocaleString(
+      'hu-HU', { timeZone: 'CET' }
+    );
     console.log(
       `User with id: ${_id} logged out at: ${date}`
     );
