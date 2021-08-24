eac1d67a6aaa69d5c68764c34f287f0278f18bc8
Jovana Jovanovic <jjovana314@gmail.com>
2021-08-24 16:35:19 +0200

    methods updated, _id used instead of id

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,55 +6,69 @@ import { AproveDto } from './dto/aprove.interface.dto';
 
 @Controller('admin')
 export class AdminController {
-    constructor(private readonly adminService: AdminService) {}
+  constructor(private readonly adminService: AdminService) { }
 
-    @Post('addComment')
-    async visitorReview(@Body() visitorData: AdminDto): Promise<Admin> {
-        const result = await this.adminService.visitorReview(visitorData);
-        await this.adminService.addLogs(visitorData.id, 'added');
-        return await result;
-    }
+  @Post('addComment')
+  async visitorReview(@Body() visitorData: AdminDto): Promise<Admin> {
+    const result = await this.adminService.visitorReview(visitorData);
+    await this.adminService.addLogs(visitorData.id, 'added');
+    return await result;
+  }
 
-    @Get('filterByNickname')
-    async findCommentByNickname(@Query() inputNick): Promise<Admin> {
-        // should be called like this: http://localhost:3000/admin/filterByNickName?nickname=visitorNick
-        const result = await this.adminService.findCommentByNickname(inputNick.nickname);
-        await this.adminService.commentExist(result);
-        return await result;
-    }
+  @Get('filterByNickname')
+  async findCommentByNickname(@Query() inputNick): Promise<Admin> {
+    // should be called like this: http://localhost:3000/admin/filterByNickName?nickname=visitorNick
+    const result = await this.adminService.findCommentByNickname(inputNick.nickname);
+    await this.adminService.commentExist(result);
+    return await result;
+  }
 
-    @Get('filterById/:id')
-    async findCommentById(@Param('id') id: string): Promise<Admin> {
-        const result = await this.adminService.findCommentById(id);
-        await this.adminService.commentExist(result);
-        return await result;
-    }
+  @Get('filterById/:_id')
+  async findCommentById(
+    @Param('_id') _id: string
+  ): Promise<Admin> {
+    const result = await this.adminService.findCommentById(_id);
+    await this.adminService.commentExist(result);
+    return await result;
+  }
 
-    @Get('filterByDate/:dateComment')
-    async findCommentsByDate(@Param('dateComment') dateComment: string): Promise<Admin[]> {
-        const result = await this.adminService.findCommentsByDate(dateComment);
-        await this.adminService.commentExist(result);
-        return await result;
-    }
+  @Get('filterByDate/:dateComment')
+  async findCommentsByDate(
+    @Param('dateComment') dateComment: string
+  ): Promise<Admin[]> {
+    const result = await this.adminService.findCommentsByDate(
+      dateComment
+    );
+    await this.adminService.commentExist(result);
+    return await result;
+  }
 
-    @Patch('update/:id')
-    async updateComment(@Param('id') commentId: string, @Body() input: AdminDto): Promise<Admin> {
-        const result = await this.adminService.updateComment(commentId, input);
-        await this.adminService.addLogs(commentId, 'updated');
-        return await result;
-        
-    }
-    @Patch('aprove/:id')
-    async aproveComment(@Param('id') commentId: string, @Body() input: AproveDto) {
-        const result = await this.adminService.aproveComment(commentId, input.aprove);
-        await this.adminService.addLogs(commentId, 'aproved');
-        return await result;
-    }
+  @Patch('update/:_id')
+  async updateComment(
+    @Param('_id') commentId: string, @Body() input: AdminDto
+  ): Promise<Admin> {
+    const result = await this.adminService.updateComment(
+      commentId, input
+    );
+    await this.adminService.addLogs(commentId, 'updated');
+    return await result;
 
-    @Delete()
-    async deleteComment(@Query() input) {
-        // http://localhost:3000/admin?id=ID
-        await this.adminService.removeComment(input.id);
-        await this.adminService.addLogs(input.id, 'removed');
-    }
+  }
+  @Patch('aprove/:_id')
+  async aproveComment(
+    @Param('_id') commentId: string, @Body() input: AproveDto
+  ): Promise<Admin> {
+    const result = await this.adminService.aproveComment(
+      commentId, input.aprove
+    );
+    await this.adminService.addLogs(commentId, 'aproved');
+    return await result;
+  }
+
+  @Delete()
+  async deleteComment(@Query() input) {
+    // http://localhost:3000/admin?id=ID  -- id is primary key (_id)
+    await this.adminService.removeComment(input.id);
+    await this.adminService.addLogs(input.id, 'removed');
+  }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -7,51 +7,55 @@ import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class AdminService {
-    constructor(
-        @InjectModel('Admin') private readonly adminModel: Model<Admin> 
-    ) {}
+  constructor(
+    @InjectModel('Admin') private readonly adminModel: Model<Admin>
+  ) { }
 
-    async visitorReview(visitorData: AdminDto): Promise<Admin> {
-        const newVisitor = new this.adminModel(visitorData);
-        return await newVisitor.save();
+  async visitorReview(visitorData: AdminDto): Promise<Admin> {
+    const newVisitor = new this.adminModel(visitorData);
+    return await newVisitor.save();
+  }
+
+  async findCommentById(_id: string) {
+    return await this.adminModel.findById(_id);
+  }
+
+  async findCommentByNickname(nickaname: string): Promise<Admin> {
+    return await this.adminModel.findOne(
+      { visitorNick: nickaname }
+    );
+  }
+
+  async aproveComment(commentId: string, aprove: boolean) {
+    return await this.adminModel.findByIdAndUpdate(
+      commentId,
+      { $set: { aproved: aprove } }
+    );
+  }
+
+  async findCommentsByDate(dateComment: string): Promise<Admin[]> {
+    const promises = await this.adminModel.find(
+      { dateAdded: dateComment }
+    );
+    return await Promise.all(promises);     // array of Promise objects
+  }
+
+  async updateComment(_id: string, comment: AdminDto): Promise<Admin> {
+    return await this.adminModel.findByIdAndUpdate(_id, comment);
+  }
+
+  async removeComment(_id: string) {
+    await this.adminModel.findByIdAndRemove(_id);
+  }
+
+  async commentExist(result) {
+    if (!result) {
+      throw new NotFoundException('Comment not found.');
     }
+  }
 
-    async findCommentById(id: string) {
-        return await this.adminModel.findOne( {id: id});
-    }
-
-    async findCommentByNickname(nickaname: string): Promise<Admin> {
-        return await this.adminModel.findOne( { visitorNick: nickaname } );
-    }
-
-    async aproveComment(commentId: string, aprove: boolean) {
-        return await this.adminModel.findOneAndUpdate(
-            {id: commentId},
-            {$set: { aproved: aprove }}
-        );
-    }
-
-    async findCommentsByDate(dateComment: string): Promise<Admin[]> {
-        const promises = await this.adminModel.find({ dateAdded: dateComment });
-        return await Promise.all(promises);     // array of Promise objects
-    }
-
-    async updateComment(id: string, comment: AdminDto): Promise<Admin> {
-        return await this.adminModel.findOneAndUpdate({ id: id }, comment);
-    }
-
-    async removeComment(id: string) {
-        await this.adminModel.findOneAndRemove({ id: id });
-    }
-
-    async commentExist(result) {
-        if (!result) {
-           throw new NotFoundException('Comment not found.');
-        }
-   }
-
-    async addLogs(id: string, action: string) {
-        var date = new Date().toLocaleString('hu-HU', {timeZone: 'CET'});
-        await console.log('Comment ' + action + ': ' + date + ' --- id: ' + id);
-    }
+  async addLogs(id: string, action: string) {
+    var date = new Date().toLocaleString('hu-HU', { timeZone: 'CET' });
+    await console.log(`Comment ${action}: ${date} --- id: ${id}`);
+  }
 }
